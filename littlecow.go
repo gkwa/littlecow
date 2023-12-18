01b141078d94c01c7de30a0cfe192f21ae9f35d9
Taylor Monacelli <taylormonacelli@gmail.com>
2023-12-17 16:29:36 -0800

    working
    OK

--- littlecow.go
+++ littlecow.go
@@ -2,14 +2,24 @@ package littlecow
 
 import (
 	"log/slog"
+	"os"
 	"path/filepath"
 )
 
 func Main() int {
-	slog.Debug("littlecow", "test", true)
+	opts := OpinionatedHandlerOptions(slog.LevelDebug, Unmodified)
+	handler := slog.NewTextHandler(os.Stderr, opts)
+	logger := slog.New(handler)
+
+	slog.SetDefault(logger)
+
+	slog.Debug("debug", "debug", "debug")
+	slog.Error("error", "error", "error")
 
 	return 0
 }
+
+type ReplaceFunc func(groups []string, attr slog.Attr) slog.Attr
 
 func setPartialPath(source *slog.Source) {
 	fileName := filepath.Base(source.File)
@@ -18,13 +28,13 @@ func setPartialPath(source *slog.Source) {
 	source.File = filepath.Join(parentDir, fileName)
 }
 
-func removeTimestamp(groups []string, attr *slog.Attr) {
+func _removeTimestamp(groups []string, attr *slog.Attr) {
 	if attr.Key == slog.TimeKey && len(groups) == 0 {
 		*attr = slog.Attr{}
 	}
 }
 
-func truncateSourcePath(groups []string, attr *slog.Attr) {
+func _truncateSourcePath(groups []string, attr *slog.Attr) {
 	if attr.Key == slog.SourceKey {
 		source, _ := attr.Value.Any().(*slog.Source)
 		if source != nil {
@@ -33,17 +43,30 @@ func truncateSourcePath(groups []string, attr *slog.Attr) {
 	}
 }
 
-func Replace(groups []string, attr slog.Attr) slog.Attr {
-	removeTimestamp(groups, &attr)
-	truncateSourcePath(groups, &attr)
+func RemoveTimestampAndTruncateSource(groups []string, attr slog.Attr) slog.Attr {
+	_removeTimestamp(groups, &attr)
+	_truncateSourcePath(groups, &attr)
 	return attr
 }
 
-func Replace2(groups []string, attr slog.Attr) slog.Attr {
-	truncateSourcePath(groups, &attr)
+func RemoveTimestamp(groups []string, attr slog.Attr) slog.Attr {
+	_removeTimestamp(groups, &attr)
 	return attr
 }
 
-func Replace3(groups []string, attr slog.Attr) slog.Attr {
+func TruncateSourcePath(groups []string, attr slog.Attr) slog.Attr {
+	_truncateSourcePath(groups, &attr)
 	return attr
+}
+
+func Unmodified(groups []string, attr slog.Attr) slog.Attr {
+	return attr
+}
+
+func OpinionatedHandlerOptions(level slog.Level, replaceFunc ReplaceFunc) *slog.HandlerOptions {
+	return &slog.HandlerOptions{
+		AddSource:   true,
+		Level:       level,
+		ReplaceAttr: replaceFunc,
+	}
 }
